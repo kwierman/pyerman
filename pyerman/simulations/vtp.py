2c7b62340c4ce6b4ace2018838223d8939e98fe4
Kevin Wierman <kwierman@gmail.com>
2016-03-28 11:52:17 -0400

    Adding in More VTP Support

--- pyerman/simulations/vtp.py
+++ pyerman/simulations/vtp.py
@@ -1,15 +1,49 @@
 import vtk
 
 
-class VTKAnalysisObject:
-    def onInit(self):
-        pass
-    def onBuild(self):
-        pass
-    def onFinish(self):
+class PointData:
+    def __init__(self):
+        self.x=0.0
+        self.y=0.0
+        self.z=0.0
+        self.data = {}
 
 
-def readinTrackData(filename=""):
-    reader = vtk.XMLPolyDataReader()
-    reader.SetFileName(fileaname)
-    reader.Update()
+class VTPDataGenerator():
+    
+    def __init__(self, filename, array_names=[]):
+        self.reader = vtk.vtkXMLPolyDataReader()
+        self.reader.SetFileName(filename)
+        self.reader.Update()
+
+        self.polydata = self.reader.GetOutput()
+        self.n_points = self.polydata.GetNumberOfPoints()
+        self.current_point = 0
+        self.point = PointData()
+
+        #initialize the array
+        self.pointdata = self.polydata.GetPointData()
+        n_arrays = self.pointdata.GetNumberOfArrays()
+        self.array_map={}
+        for name in array_names:
+            self.point.data[name]=None
+            for index in range(n_arrays):
+                arr = self.pointdata.GetArray(index)
+                if self.pointdata.GetArrayName(index)== name:
+                    self.array_map[name]=arr
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        return self.next()
+
+    def next(self):
+        if self.current_point+1 == self.n_points:
+            raise StopIteration()
+        self.point.x, self.point.y, self.point.z = self.polydata.GetPoint(self.current_point)
+        #iterate through the data and pick out the relevent arrays
+        for name in self.point.data:
+            self.point.data[name] = self.array_map[name].GetTuple1(self.current_point)
+        self.current_point+=1
+        return self.point
